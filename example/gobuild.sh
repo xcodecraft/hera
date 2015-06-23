371f3f1c732edba0fdbf663fbee759098b38575b
duanbingying <duanbingying@360.cn>
2015-06-23 17:18:02 +0800

    add zdaemon

--- example/gobuild.sh
+++ example/gobuild.sh
@@ -5,11 +5,11 @@ export GOPATH=$ROOT_DIR
 export GOBIN=$ROOT_DIR/bin
 
 echo "Init env..."
-echo "GOPATH is :$GOPATH" 
-echo "GOBIN  is :$GOBIN" 
+echo "GOPATH is :$GOPATH"
+echo "GOBIN  is :$GOBIN"
 
 echo "It is deploying hera, please wait..."
-go get -u github.com/xcodecraft/hera
+go get github.com/xcodecraft/hera
 echo "hera deploy success."
 
 rm -rf ./pkg/ ./bin/
@@ -18,7 +18,9 @@ echo "It is complie your prj, please wait..."
 go install main
 echo "complie success."
 
-ln -s $ROOT_DIR/conf/nginx.conf /usr/local/nginx/conf/include/example_duanbingying_nginx.conf
-exec nginx -s reload
+sudo rm -rf /usr/local/nginx/conf/include/example_duanbingying_nginx.conf
+sudo ln -s $ROOT_DIR/conf/nginx.conf /usr/local/nginx/conf/include/example_duanbingying_nginx.conf
 
-exec ./bin/main
+sudo nginx -s reload
+
+/usr/local/bin/python  /usr/local/bin/zdaemon  -C $ROOT_DIR/conf/zd.xml start
