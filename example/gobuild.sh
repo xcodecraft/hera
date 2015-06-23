b65c2b214d2d595306f19150cb2063f17cf9a201
duanbingying <duanbingying@360.cn>
2015-06-23 17:41:59 +0800

    gobuild shell 完善

--- example/gobuild.sh
+++ example/gobuild.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 ROOT_DIR=`pwd`
 
+PRJ='hera_example'
+
 export GOPATH=$ROOT_DIR
 export GOBIN=$ROOT_DIR/bin
 
+echo "**********************************Strat****************************"
 echo "Init env..."
 echo "GOPATH is :$GOPATH"
 echo "GOBIN  is :$GOBIN"
@@ -13,14 +16,28 @@ go get github.com/xcodecraft/hera
 echo "hera deploy success."
 
 rm -rf ./pkg/ ./bin/
-
 echo "It is complie your prj, please wait..."
 go install main
 echo "complie success."
 
-sudo rm -rf /usr/local/nginx/conf/include/example_duanbingying_nginx.conf
-sudo ln -s $ROOT_DIR/conf/nginx.conf /usr/local/nginx/conf/include/example_duanbingying_nginx.conf
-
-sudo nginx -s reload
-
-/usr/local/bin/python  /usr/local/bin/zdaemon  -C $ROOT_DIR/conf/zd.xml start
+case $1 in
+    start)
+        echo "$PRJ start:"
+        sudo rm -rf /usr/local/nginx/conf/include/$PRJ.conf
+        sudo ln -s $ROOT_DIR/conf/nginx.conf /usr/local/nginx/conf/include/$PRJ.conf
+        sudo nginx -s reload
+        /usr/local/bin/python  /usr/local/bin/zdaemon  -C $ROOT_DIR/conf/zd.xml start
+        ;;
+    stop)
+        echo "$PRJ stop:"
+        /usr/local/bin/python  /usr/local/bin/zdaemon  -C $ROOT_DIR/conf/zd.xml stop
+        ;;
+    restart)
+        echo "$PRJ restart:"
+        sudo rm -rf /usr/local/nginx/conf/include/$PRJ.conf
+        sudo ln -s $ROOT_DIR/conf/nginx.conf /usr/local/nginx/conf/include/$PRJ.conf
+        sudo nginx -s reload
+        /usr/local/bin/python  /usr/local/bin/zdaemon  -C $ROOT_DIR/conf/zd.xml restart
+        ;;
+esac
+echo "**********************************End****************************"
